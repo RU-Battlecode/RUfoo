d1bc1752f46a24b2ff9f55bd7ff51ecd15363f7e
imachumphries <imachumphries@me.com>
2017-01-10 02:52:21 -0500

    Added scout management to gardener

--- logic/GardenerLogic.java
+++ logic/GardenerLogic.java
@@ -2,12 +2,42 @@ package RUfoo.logic;
 
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
+import battlecode.common.RobotType;
 
 public class GardenerLogic extends RobotLogic {
 
+	private static final Direction[] SCOUT_BUILD_DIRECTIONS = { 
+			Direction.getNorth(),
+			Direction.getEast().rotateRightDegrees(45),
+			Direction.getWest().rotateLeftDegrees(45) };
+
+	private int scoutsMade;
+	
+	public GardenerLogic() {
+		scoutsMade = 0;
+	}
+	
 	@Override
 	public void logic() {
 		navManager.dodgeBullets();
+		if (scoutsMade < 2) {
+			build(RobotType.SCOUT);
+		}
 	}
+	
+	void build(RobotType type) {
+		Direction dir = SCOUT_BUILD_DIRECTIONS[scoutsMade % SCOUT_BUILD_DIRECTIONS.length];
+		if (rc.canBuildRobot(type, dir)) {
+			try {
+				rc.buildRobot(type, dir);
+				if (type == RobotType.SCOUT) {
+					scoutsMade++;
+				}
+			} catch (GameActionException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 
 }
