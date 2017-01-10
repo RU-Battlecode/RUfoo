58708c4998de0faaa49e3ca599417d9c8b4f1a3a
imachumphries <imachumphries@me.com>
2017-01-10 03:57:44 -0500

    Added dumb soldier script

--- logic/SoldierLogic.java
+++ logic/SoldierLogic.java
@@ -1,10 +1,18 @@
 package RUfoo.logic;
 
+import battlecode.common.RobotInfo;
+
 public class SoldierLogic extends RobotLogic {
 
 	@Override
 	public void logic() {
-		
+		RobotInfo target = combatManager.findTarget();
+		if (target != null) {
+			navManager.moveAggressively(target.location);
+			combatManager.singleShotAttack(target);
+		} else {
+			navManager.dodgeBullets();
+		}
 	}
 	
 }
