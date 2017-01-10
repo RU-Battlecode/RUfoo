9535c796ff0fd669a1687a36a8750101f7765580
imachumphries <imachumphries@me.com>
2017-01-09 22:36:13 -0500

    Make gardeners move randomly and dodge bullets

--- logic/GardenerLogic.java
+++ logic/GardenerLogic.java
@@ -1,10 +1,13 @@
 package RUfoo.logic;
 
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+
 public class GardenerLogic extends RobotLogic {
 
 	@Override
 	public void logic() {
-		
+		navManager.dodgeBullets();
 	}
 
 }
