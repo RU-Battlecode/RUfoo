ed4e56f09b16e6745c05848823ba36841731e0fa
imachumphries <imachumphries@me.com>
2017-01-10 03:58:02 -0500

    Fix dumb scout script

--- logic/ScoutLogic.java
+++ logic/ScoutLogic.java
@@ -1,15 +1,11 @@
 package RUfoo.logic;
 
-import RUfoo.managers.Combat;
-
 public class ScoutLogic extends RobotLogic {
 
-	private Combat combatManager;
-	
 	@Override
 	public void logic() {
 		combatManager.singleShotAttack();
-		//navManager.dodgeBullets();
+		navManager.dodgeBullets();
 	}
 
 }
