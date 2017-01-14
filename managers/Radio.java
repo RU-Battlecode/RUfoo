35ace48e60a788b2af6d893115f2d8828ae4aa05
imachumphries <imachumphries@me.com>
2017-01-14 01:39:41 -0500

    Formatting

--- managers/Radio.java
+++ managers/Radio.java
@@ -8,9 +8,8 @@ import battlecode.common.RobotController;
 public class Radio {
 
 	private RobotController rc;
-	
+
 	public static final int TREE_CHANNEL = 0;
-	
 
 	public Radio(RobotController _rc) {
 		rc = _rc;
@@ -27,29 +26,30 @@ public class Radio {
 		}
 		return freeIndex;
 	}
-	
+
 	public MapLocation readTreeChannel() {
-		
+
 		try {
 			int msg = rc.readBroadcast(TREE_CHANNEL);
-			
+
 			return msg == 0 ? null : intToMapLocation(msg);
-			
+
 		} catch (GameActionException e) {
 			e.printStackTrace();
 			return null;
 		}
-		
 	}
-	
+
 	public void requestCutTreeAt(MapLocation loc) {
 		try {
-			rc.broadcast(TREE_CHANNEL, mapLocationToInt(loc));
+			if (rc.readBroadcast(TREE_CHANNEL) == 0) {
+				rc.broadcast(TREE_CHANNEL, mapLocationToInt(loc));
+			}
 		} catch (GameActionException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void taskComplete(int channel) {
 		try {
 			rc.broadcast(channel, 0);
@@ -57,14 +57,13 @@ public class Radio {
 			e.printStackTrace();
 		}
 	}
-	
+
 	int mapLocationToInt(MapLocation loc) {
-		return Math.round(loc.x) << 16 | Math.round(loc.y);		 
+		return Math.round(loc.x) << 16 | Math.round(loc.y);
 	}
-	
+
 	MapLocation intToMapLocation(int data) {
 		return new MapLocation(data >> 16, data & 0xffff);
 	}
 
-	
 }
