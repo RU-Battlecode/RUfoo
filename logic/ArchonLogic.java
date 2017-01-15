3668f877cb1057d76513c81c10932a4481a3e52d
imachumphries <imachumphries@me.com>
2017-01-15 02:20:59 -0500

    Archon: Better base building!

--- logic/ArchonLogic.java
+++ logic/ArchonLogic.java
@@ -1,76 +1,74 @@
 package RUfoo.logic;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import RUfoo.managers.Navigation;
-import RUfoo.util.Util;
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
+import battlecode.common.GameConstants;
 import battlecode.common.RobotController;
-import battlecode.common.RobotInfo;
 import battlecode.common.RobotType;
 import battlecode.common.Team;
 import battlecode.common.TreeInfo;
 
 public class ArchonLogic extends RobotLogic {
 
-	private Direction buildDir;
+	// Prioritized build directions
+	private List<Direction> buildDirs = new ArrayList<>(Arrays.asList(new Direction[]{
+			Direction.getNorth(),
+			Navigation.NORTH_EAST,
+			Navigation.NORTH_WEST,
+			Direction.getEast(),
+			Direction.getWest(),
+			Navigation.SOUTH_EAST,
+			Navigation.SOUTH_WEST, }));
+	
+	private float buildOffset;
 	
 	public ArchonLogic(RobotController _rc) {
 		super(_rc);
-		buildDir = nav.randomDirection();
+
+		Direction pointAt = rc.getLocation().directionTo(combat.getClosestEnemySpawn());
+		buildOffset = buildDirs.get(0).degreesBetween(pointAt); 
 	}
 
 	@Override
 	public void logic() {
-		RobotInfo[] robots = rc.senseNearbyRobots();
-		List<RobotInfo> gardeners = new ArrayList<>();
-
-		for (RobotInfo info : robots) {
-			if (info.getTeam() == rc.getTeam()) {
-				if (info.getType() == RobotType.GARDENER) {
-					gardeners.add(info);
-				}
-			} else {
-				// Sensed other team...
+		buildBase();
+	}
+	
+	void buildBase() {
+		Direction built = null;
+		for (Direction dir : buildDirs) {
+			Direction adjusted = dir.rotateLeftDegrees(buildOffset);
+			if (buildGardener(adjusted)) {
+				built = dir;
+				break;
 			}
 		}
-
-		// Do we need to build more gardeners?
-		if (rc.isBuildReady() && gardeners.size() < 6) {
-			buildGardener();
-		}
-
-		nav.dodgeBullets();
-		nav.runAway();
 		
-		try {
-			if (!rc.onTheMap(rc.getLocation().add(buildDir, rc.getType().sensorRadius / 2)) ||
-					!rc.canMove(buildDir, rc.getType().strideRadius)) {
-				buildDir = personality.getIsLeftHanded() ?  buildDir.rotateLeftDegrees(45) : buildDir.rotateRightDegrees(45);
-			}
-		} catch (GameActionException e) {
-			
-			e.printStackTrace();
+		if (built != null) { 
+			buildDirs.remove(built);
 		}
 		
-		if (!rc.isBuildReady()) {
-			nav.moveBest(buildDir);
+		if (rc.getRoundNum() == 600 && rc.getTeamVictoryPoints() < GameConstants.VICTORY_POINTS_TO_WIN * 0.80f) {
+			for (int i = 0; i < 5; i++) buildDirs.add(nav.randomDirection());
 		}
-		
-		orderClearTrees();
 	}
 
-	void buildGardener() {
-		Direction dir = Util.randomChoice(Navigation.DIRECTIONS);
+	boolean buildGardener(Direction dir) {
 		if (rc.canBuildRobot(RobotType.GARDENER, dir)) {
 			try {
 				rc.buildRobot(RobotType.GARDENER, dir);
+				return true;
 			} catch (GameActionException e) {
 				e.printStackTrace();
 			}
 		}
+		
+		return false;
 	}
 	
 	void orderClearTrees() {
