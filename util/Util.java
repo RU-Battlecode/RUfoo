d23d41a0c47ed45f0aa9b91775a6d6a18958ef62
imachumphries <imachumphries@me.com>
2017-01-27 23:01:08 -0500

    Fix logic error

--- util/Util.java
+++ util/Util.java
@@ -83,10 +83,8 @@ public final class Util {
 		if (dir1 == null || dir2 == null) {
 			return false;
 		}
-		float degreesBetween = dir1.degreesBetween(dir2);
-
-		return (degreesBetween >= 0 && degreesBetween <= degreesOk)
-				|| (degreesBetween >= 360 - degreesOk && degreesBetween <= 360);
+		
+		return Math.abs(dir1.degreesBetween(dir2)) <= degreesOk;
 	}
 
 	/**
