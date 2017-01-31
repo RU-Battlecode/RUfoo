f09ed46361499a8f00ea237e2c799347b29079ab
imachumphries <imachumphries@me.com>
2017-01-30 23:09:40 -0500

    Less donation

--- logic/RobotLogic.java
+++ logic/RobotLogic.java
@@ -20,8 +20,8 @@ import battlecode.common.TreeInfo;
  */
 public abstract class RobotLogic {
 
-	private static final float DONATE_AFTER = 500; // bullets
-	private static final float DONATE_PERCENTAGE = 0.10f;
+	private static final float DONATE_AFTER = 400; // bullets
+	private static final float DONATE_PERCENTAGE = 0.03f;
 
 	protected RobotController rc;
 	protected Nav nav;
